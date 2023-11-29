606eb767affec990c75496cbc93bc87f46bd6420
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-28 22:04:59 -0300

    Cap 13 - Tipos de datos en Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,9 +1,29 @@
-# Cap 11 - Ejercicio Propuesto - uso de variable en Python
-nombre="Juan perez"
-telefono=55621817
-email="jperez@mail.com"
+# Cap 13 - Tipos de datos en Python
+"""
+Tipos de datos:
 
-print(nombre)
-print(telefono)
-print(email)
+Numeric:(integer, float, Complex Number)
+Dictionary
+Boolean
+Set
+Sequense Type:(String, List, Tuple)
+"""
+x = 10
+print(x)
+print(type(x), '\n')  # class 'int'
 
+x = "Hola Mundo"
+print(x)
+print(type(x), '\n')  # class 'str'
+
+x: str = "Hola Mundo2"  # Tambien podemos agregar una pista del tipo de dato (hint = pista o indicacion)
+print(x)
+print(type(x), '\n')  # class 'str'
+
+x: str = 10  # Esto no define el tipo de la variable (hint = pista o indicacion)
+print(x)
+print(type(x), '\n')  # class 'int'
+
+x = True  # o False. Respetar mayusculas inicial
+print(x)
+print(type(x), '\n')  # class 'bool'
