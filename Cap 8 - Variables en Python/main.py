20ab1b8d1d4bf2ab1ab28d642b6260c12eb03d95
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-28 22:15:04 -0300

    Cap 14 - Resumen Tipos de datos en Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,29 +1,37 @@
-# Cap 13 - Tipos de datos en Python
+# Cap 14 - Resumen Tipos de datos en Python
 """
 Tipos de datos:
 
-Numeric:(integer, float, Complex Number)
+*Numeric:(*integer, *float, Complex Number)
 Dictionary
-Boolean
+*Boolean
 Set
-Sequense Type:(String, List, Tuple)
+Sequense Type:(*String, List, Tuple)
 """
+#Tipo int
 x = 10
 print(x)
 print(type(x), '\n')  # class 'int'
 
+#Tipo float
+x= 14.5
+print(x)
+print(type(x), '\n')  # class 'int'
+
+#Tipo String
 x = "Hola Mundo"
 print(x)
 print(type(x), '\n')  # class 'str'
 
-x: str = "Hola Mundo2"  # Tambien podemos agregar una pista del tipo de dato (hint = pista o indicacion)
-print(x)
-print(type(x), '\n')  # class 'str'
-
-x: str = 10  # Esto no define el tipo de la variable (hint = pista o indicacion)
-print(x)
-print(type(x), '\n')  # class 'int'
-
+#Tipo Bool
 x = True  # o False. Respetar mayusculas inicial
 print(x)
 print(type(x), '\n')  # class 'bool'
+x = False
+print(x)
+print(type(x), '\n')  # class 'bool'
+
+
+
+
+
