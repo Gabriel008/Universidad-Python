0fa439e5b8ee24ef807488c5412ae8c7d411cf0b
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-29 20:47:59 -0300

    Cap 15 - Manejo de cadenas en Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,37 +1,5 @@
-# Cap 14 - Resumen Tipos de datos en Python
-"""
-Tipos de datos:
+# Cap 15 - Manejo de cadenas en Python
 
-*Numeric:(*integer, *float, Complex Number)
-Dictionary
-*Boolean
-Set
-Sequense Type:(*String, List, Tuple)
-"""
-#Tipo int
-x = 10
-print(x)
-print(type(x), '\n')  # class 'int'
-
-#Tipo float
-x= 14.5
-print(x)
-print(type(x), '\n')  # class 'int'
-
-#Tipo String
-x = "Hola Mundo"
-print(x)
-print(type(x), '\n')  # class 'str'
-
-#Tipo Bool
-x = True  # o False. Respetar mayusculas inicial
-print(x)
-print(type(x), '\n')  # class 'bool'
-x = False
-print(x)
-print(type(x), '\n')  # class 'bool'
-
-
-
-
-
+miGrupoFavorito = "Aerosmith"
+comentario = "The Best Rock Band"
+print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)  # + como concatenacion basica
