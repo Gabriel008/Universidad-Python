920a9b1263cddc119e34ba8eb4e8dc10c3cbb571
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-28 21:24:51 -0300

    Cap 11 - Ejercicio propuesto, uso de variables en Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,12 +1,9 @@
-# Cap 10 - Direccion de memoria y Variables en Python
-x = 10 #Cada uno de estos valores es un literal. Es decir, literal 10 asignado a la variable x
-y = 2 #Pero como se a comentado estas literales se almacenan en una posicion de memoria y la variable apunta a esa direccion de memoria
-z = x + y
-print(x)
-print(y)
-print(z)
+# Cap 11 - Ejercicio Propuesto - uso de variable en Python
+nombre="Juan perez"
+telefono=55621817
+email="jperez@mail.com"
 
-#Para saber la direccion de memoria a la que apunta la variable se utiliza la funcion id de la siguiente manera:
-print(id(x)) #posicion de memoria donde se encuentra el valor que almacena la variable x en este caso la literal 10
-print(id(y))
-print(id(z))
+print(nombre)
+print(telefono)
+print(email)
+
