8f2dee47dd279f5ed0b4440cc6a58d16c2a026fb
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-28 21:05:26 -0300

    Cap 9 - Variables parte II Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,14 +1,7 @@
-
-miVariable = "Hola desde Python"
-print(miVariable)
-print(miVariable)
-print(miVariable)
-
-#En Python no es necesario indicar el tipo por lo que en cualquier momento podemos cambiar su valor
-miVariable = 2
-print(miVariable)
-print(miVariable)
-print(miVariable)
-
-miVariable = 10
-print(miVariable)
+# Cap 9 - Variables en Python Parte II
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
