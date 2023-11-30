d1c7c893ebd31c2064dd6812d9d0fa5bc63e7500
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-29 21:03:52 -0300

    Cap 16 - Mas manejo de cadenas en Python

--- Cap 8 - Variables en Python/main.py
+++ Cap 8 - Variables en Python/main.py
@@ -1,5 +1,16 @@
-# Cap 15 - Manejo de cadenas en Python
+# Cap 16 - Mas manejo de cadenas en Python
 
 miGrupoFavorito = "Aerosmith"
 comentario = "The Best Rock Band"
-print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)  # + como concatenacion basica
+# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)  # + como concatenacion basica
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+numero1 = "1"
+numero2 = "2"
+print(numero1 + numero2)  # concatenacion
+print("Suma:", int(numero1) + int(numero2))  # cast
+
+numero1 = 1
+numero2 = 2
+print(numero1 + numero2)  # suma
+# A sto se le llama sobrecarga de operadores
