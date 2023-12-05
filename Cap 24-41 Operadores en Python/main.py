a07d230197b9a7166175f4d8beeb044add2c4c50
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-04 21:32:58 -0300

    Cap 24 - Operadores Aritmeticos en Python

---
+++ Cap 24-41 Operadores en Python/main.py
@@ -0,0 +1,20 @@
+# Cap 24 - Operadores Aritmeticos en Python
+"""
+
+Operador        |       Simbolo
+--------------------------------
+Adicion                     +
+resta                       -
+Multiplicacion              *
+Division                    /
+Division entera             //
+Exponente                   **
+Mod                         %
+
+"""
+
+operandoA = 3
+operandoB = 2
+suma = operandoA + operandoB
+#print('Resultado de la suma:', suma)
+print(f'Resultado suma: {suma}')
