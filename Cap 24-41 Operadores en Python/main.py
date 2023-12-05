ec209df1b67e910b5e5d5e4ddf86b78a304ddde3
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-04 21:47:30 -0300

    Cap 25 - Operadores Aritmeticos en Python - Parte2

--- Cap 24-41 Operadores en Python/main.py
+++ Cap 24-41 Operadores en Python/main.py
@@ -1,20 +1,41 @@
-# Cap 24 - Operadores Aritmeticos en Python
-"""
-
-Operador        |       Simbolo
---------------------------------
-Adicion                     +
-resta                       -
-Multiplicacion              *
-Division                    /
-Division entera             //
-Exponente                   **
-Mod                         %
-
-"""
+# Cap 25 - Operadores Aritmeticos en Python - Parte2
 
 operandoA = 3
 operandoB = 2
+# ----------------------------------------#
+
 suma = operandoA + operandoB
-#print('Resultado de la suma:', suma)
+# print('Resultado de la suma:', suma)
 print(f'Resultado suma: {suma}')
+
+# ----------------------------------------#
+
+resta = operandoA - operandoB
+print(f'Resultado de la resta: {resta}')
+
+# ----------------------------------------#
+
+multiplicacion = operandoA * operandoB
+print(f'Resultado de la multiplicacion: {multiplicacion}')
+
+# ----------------------------------------#
+
+division = operandoA / operandoB
+print(f'Resultado de la division: {division}')
+
+# ----------------------------------------#
+
+divEntera = operandoA // operandoB
+print(f'Resultado de la division entera: {divEntera}')
+
+# ----------------------------------------#
+
+modulo = operandoA % operandoB
+print(f'Resultado moduloa: {modulo}')
+
+# ----------------------------------------#
+
+exponente = operandoA ** operandoB
+print(f'Resultado exponenete: {exponente}')
+
+# ----------------------------------------#
