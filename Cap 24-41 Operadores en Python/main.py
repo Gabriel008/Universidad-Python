80b3396b6f6c4463d6d9b89722a2691662c51672
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-05 22:19:13 -0300

    Cap 28 - Operadores de asignacion en Python

--- Cap 24-41 Operadores en Python/main.py
+++ Cap 24-41 Operadores en Python/main.py
@@ -1,21 +1,23 @@
-# Cap 26-27 - Ejercicio: Rectangulo en Python
-"""
-Se solicita calcular el Area y el perimetro de un rectangulo, para ello debemos crear la siguientes variables
-    alto (int)
-    ancho (int)
-El usuario debe proporcionar los valores de largo y ancho y despues se debe imprimir  en el siguiente formato
-    Proporciona el alto:
-    Proporciona el ancho:
-    Area:<Area>
-    Perimetro:<perimetro>
+# Cap 28 - Operadores de asignacion en Python
 
-Las formulas para calcular el area y el perimetro de un rectangulo son:
-    Area:alto*ancho
-    perimetro:(alto + ancho)*2
-"""
+miVariable = 10
+print(miVariable)
 
-alto = int(input('Proporciona el alto del rectangulo:'))
-ancho = int(input('Proporciona el ancho del rectangulo:'))
-print(f'Area: {alto * ancho}')
-print(f'Perimetro: {(alto + ancho) * 2}')
-print('------Fin del programa------')
+miVariable = miVariable + 1
+print(miVariable)
+
+# Incremento con reasignacion
+miVariable += 1
+print(miVariable)
+
+# miVariable = miVariable-2
+miVariable -= 2
+print(miVariable)
+
+# miVariable = miVariable*3
+miVariable *= 3
+print(miVariable)
+
+# miVariable = miVariable/2
+miVariable /= 2
+print(miVariable)
