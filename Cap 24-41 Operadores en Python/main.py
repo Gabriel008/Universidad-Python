b9b11f12611a15676363eb6a6c34965c704fecb9
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-10 22:55:50 -0300

    Cap 36 - Ejercicio: Rango entre 20's y 30's en Python

--- Cap 24-41 Operadores en Python/main.py
+++ Cap 24-41 Operadores en Python/main.py
@@ -1,9 +1,21 @@
-# Cap 35 - Ejercicio: operador Not en Python
+# Cap 36 - Ejercicio: Rango entre 20's y 30's en Python
 
-vacaciones = False
-diaDescanso = False
+edad = int(input('Intruduce tu edad: '))
 
-if not (vacaciones or diaDescanso):
-    print('Tiene deberes por hacer')
+"""veintes = edad >= 20 and edad < 30
+print("20's:", veintes)
+treintas = edad >= 30 and edad < 40
+print("30's:", treintas)"""
+
+if (edad >= 20 and edad < 30) or (edad >= 30 and edad < 40):
+    print('Dentro de rango 20\'s o 30\'s')
+    """    
+        if veintes:
+            print("Dentro de los 20's")
+        elif treintas:
+            print("Dentro de los 30's")
+        else:
+            print('Fuera de rango')
+    """
 else:
-    print('Puede asistir al juego')
+    print("No esta dentro de los 20's ni 30's")
