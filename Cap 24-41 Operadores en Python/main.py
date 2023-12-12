ea327f34ed0d735819372ae38f0126e69afc8c05
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-11 22:07:59 -0300

    Cap 38-39 - Ejercicio: el mayor de dos numeros en Python

--- Cap 24-41 Operadores en Python/main.py
+++ Cap 24-41 Operadores en Python/main.py
@@ -1,23 +1,20 @@
-# Cap 37 - Sintaxis simplificada operador AND Python
-
-edad = int(input('Intruduce tu edad: '))
+# Cap 38-39 - Ejercicio: el mayor de dos numeros en Python
 
 """
-    veintes = edad >= 20 and edad < 30
-    print("20's:", veintes)
-    treintas = edad >= 30 and edad < 40
-    print("30's:", treintas)
+    Instrucciones:
+    Solicitar al usuario dos valores y determinar cual numero es el mayor
+    Solicitar al usuario dos valores :
+    numero1 (int)
+    numero2 (int)
+    Se debe imprimir el mayor de los dos numeros (La salida debe ser identica a la que sigue):
+    Proporciona ek numero1:
+    Proporciona ek numero2:
+    El numero mayor es: <numeroMayor>
 """
+numero1 = int(input('Proporciona el Numero 1:'))
+numero2 = int(input('Proporciona el Numero 2:'))
 
-if (20 <= edad < 30) or (30 <= edad < 40):
-    print('Dentro de rango 20\'s o 30\'s')
-    """    
-        if veintes:
-            print("Dentro de los 20's")
-        elif treintas:
-            print("Dentro de los 30's")
-        else:
-            print('Fuera de rango')
-    """
+if numero1 > numero2:
+    print(f'Numero 1 mayor es: {numero1}')
 else:
-    print("No esta dentro de los 20's ni 30's")
+    print(f'Numero 2 mayor es: {numero2}')
