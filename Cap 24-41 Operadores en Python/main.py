1e2f113219a8223d24effaf9c7369a52b8bb77b0
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-17 23:12:10 -0300

    Cap 40-41 - Ejercicio: Tienda de libros en Python

--- Cap 24-41 Operadores en Python/main.py
+++ Cap 24-41 Operadores en Python/main.py
@@ -1,20 +1,40 @@
-# Cap 38-39 - Ejercicio: el mayor de dos numeros en Python
+# Cap 40-41 - Ejercicio: Tienda de libros en Python
 
 """
     Instrucciones:
-    Solicitar al usuario dos valores y determinar cual numero es el mayor
-    Solicitar al usuario dos valores :
-    numero1 (int)
-    numero2 (int)
-    Se debe imprimir el mayor de los dos numeros (La salida debe ser identica a la que sigue):
-    Proporciona ek numero1:
-    Proporciona ek numero2:
-    El numero mayor es: <numeroMayor>
-"""
-numero1 = int(input('Proporciona el Numero 1:'))
-numero2 = int(input('Proporciona el Numero 2:'))
+    Proporciones los siguientes datos del libro:
+    Proporciona el nombre:
+    Proporciona el ID (numero):
+    Proporciona el precio(flotante):
+    Indicar si el envio es gratuito (True/False):
 
-if numero1 > numero2:
-    print(f'Numero 1 mayor es: {numero1}')
+    Imprimir=
+    Nombre: <>
+    Id: <>
+    Precio: <>
+    Envio Gratuito: <>
+"""
+
+print('|--------------------------------------------|')
+print(' Proporciones los siguientes datos del libro: ')
+print('|--------------------------------------------|')
+nombre = input('Proporciona el nombre: ')
+id = int(input('Proporciona el ID: '))
+precio = float(input('Proporciona el precio: '))
+envioGratuito = input('Indicar si el envio es gratuito (True/False): ')
+
+if envioGratuito == 'True':
+    envioGratuito = True
+elif envioGratuito == 'False':
+    envioGratuito = False
 else:
-    print(f'Numero 2 mayor es: {numero2}')
+    envioGratuito = 'Valor incorrecto, debe escribir True/False'
+
+print(f"""
+|----------------------------------------------------|
+    Nombre:             {nombre}
+    Id:                 {id}
+    Precio:             {precio}
+    Envio Gratuito?:    {envioGratuito}
+|----------------------------------------------------|
+""")
