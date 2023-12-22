6fb574b40485f3c95e5ea286545d755d5b8c56ef
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-21 22:55:31 -0300

    Cap 42 - If/Else en Python

---
+++ Cap42-50 Sentencias de Control/main.py
@@ -0,0 +1,10 @@
+# Cap 42 - If/Else en Python
+
+condicion = True
+
+if (condicion == True):
+    print('Condicion verdadera')
+elif (condicion == False):
+    print('Condicion Falsa')
+else:
+    print('Condicion no reconocida')
