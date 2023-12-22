5cf47fd26eda22eb5163950d3bdb98615b45b7db
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-12-21 23:13:49 -0300

    Cap 43 - Ejecucion modo debug en Python

--- Cap42-50 Sentencias de Control/main.py
+++ Cap42-50 Sentencias de Control/main.py
@@ -1,6 +1,10 @@
-# Cap 42 - If/Else en Python
+# Cap 43 - Ejecucion modo debug en Python
 
-condicion = True
+# Modo debug sirve para ver la trasa del codigo.
+# Para entrar en modo debug, seleciona un numero de linea de codigo, click derecho 'debug main'
+# Avanza en el codigo presionando F8
+
+condicion = False
 
 if (condicion == True):
     print('Condicion verdadera')
